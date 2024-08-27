7226150816177ccc4907b78d49e88b073e21d058
bhaumiksingh2000 <bhaumiksingh2000@gmail.com>
2024-08-27 04:34:20 +0000

    add progress ui component

--- src/lib/components/ui/progress/progress.svelte
+++ src/lib/components/ui/progress/progress.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
-	import { Progress as ProgressPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+  import { Progress as ProgressPrimitive } from 'bits-ui';
+  import { cn } from '$lib/utils.js';
 
-	type $$Props = ProgressPrimitive.Props;
+  type $$Props = ProgressPrimitive.Props;
 
-	let className: $$Props["class"] = undefined;
-	export let max: $$Props["max"] = 100;
-	export let value: $$Props["value"] = undefined;
-	export { className as class };
+  let className: $$Props['class'] = undefined;
+
+  export { className as class };
 </script>
 
 <ProgressPrimitive.Root
-	class={cn("bg-primary/20 relative h-2 w-full overflow-hidden rounded-full", className)}
-	{...$$restProps}
+  class={cn('bg-primary/20 relative h-2 w-full overflow-hidden rounded-full', className)}
+  {...$$restProps}
 >
-	<div
-		class="bg-primary h-full w-full flex-1 transition-all"
-		style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}
-	></div>
+  <div class="bg-primary h-full w-full flex-1 transition-all animate-progress"></div>
 </ProgressPrimitive.Root>
+
+<style>
+  @keyframes progress {
+    0% {
+      transform: translateX(-100%);
+    }
+    25% {
+      transform: translateX(25%);
+    }
+    50% {
+      transform: translateX(50%);
+    }
+    75% {
+      transform: translateX(75%);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  .animate-progress {
+    animation: progress cubic-bezier(0.55, 0.055, 0.675, 0.19) 5s infinite;
+  }
+</style>
